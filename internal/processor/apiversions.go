40c880efcbd5babe0356e90eabad885cdee75655
Khaled <khaledhegazy870@gmail.com>
2025-11-21 10:24:45 +0200

    support apiversions api key v[0-4]

--- internal/processor/apiversions.go
+++ internal/processor/apiversions.go
@@ -1,7 +1,7 @@
 package processor
 
 import (
-	"encoding/binary"
+	"bytes"
 
 	"github.com/codecrafters-io/kafka-starter-go/internal/request"
 )
@@ -10,17 +10,74 @@ type APIVersionsProcessor struct{}
 
 var _ KafkaAPIProcessor = (*APIVersionsProcessor)(nil)
 
-const (
-	NoError            = 0
-	UnsupportedVersion = 35
-)
-
-func (avkp *APIVersionsProcessor) GetRequestAPIKey() uint16 {
-	return 18
+type APIKey struct {
+	apiKey     uint16
+	minVersion uint16
+	maxVersion uint16
+	tagBuffer  uint8
 }
 
-func (avkp *APIVersionsProcessor) GenerateResponseBody(request *request.Request) ([]byte, error) {
-	buf := make([]byte, 2)
-	binary.BigEndian.PutUint16(buf, UnsupportedVersion)
-	return buf, nil
+type APIVersionsResponse struct {
+	errorCode      uint16
+	apiKeys        []APIKey
+	throttleTimeMs uint32
+	tagBuffer      uint8
+}
+
+const (
+	// Error Codes
+	NoErrorCode                 = 0
+	UnsupportedVersionErrorCode = 35
+
+	// API key
+	APIVersionsAPIKey = 18
+)
+
+var supportedAPIKeys = []APIKey{
+	{
+		apiKey:     APIVersionsAPIKey,
+		minVersion: 0,
+		maxVersion: 4,
+		tagBuffer:  0,
+	},
+}
+
+func (avp *APIVersionsProcessor) GetRequestAPIKey() uint16 {
+	return APIVersionsAPIKey
+}
+
+func (avp *APIVersionsProcessor) GenerateResponseBody(req *request.Request) ([]byte, error) {
+	var errorCode uint16
+	if req.RequestAPIVersion <= 4 {
+		errorCode = NoErrorCode
+	} else {
+		errorCode = UnsupportedVersionErrorCode
+	}
+	resp := APIVersionsResponse{
+		errorCode:      errorCode,
+		apiKeys:        supportedAPIKeys,
+		throttleTimeMs: 0,
+		tagBuffer:      0,
+	}
+	buf := new(bytes.Buffer)
+	request.WriteUint16(buf, resp.errorCode)
+
+	// TODO: Revisit unsigned variant sizes
+	// size of api keys array
+	request.WriteUint8(buf, uint8(len(resp.apiKeys)+1))
+
+	for _, apiKey := range resp.apiKeys {
+		request.WriteUint16(buf, apiKey.apiKey)
+		request.WriteUint16(buf, apiKey.minVersion)
+		request.WriteUint16(buf, apiKey.maxVersion)
+		request.WriteUint8(buf, apiKey.tagBuffer)
+	}
+
+	// Throttle in milliseconds
+	request.WriteUint32(buf, resp.throttleTimeMs)
+
+	// Tag Buffer (empty)
+	request.WriteUint8(buf, resp.tagBuffer)
+
+	return buf.Bytes(), nil
 }
